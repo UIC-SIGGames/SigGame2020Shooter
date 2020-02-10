db0e0c9a469a63602375b7c221abe2352cf2ab9a
Chris <55300770+pizzatree@users.noreply.github.com>
2020-02-10 17:54:38 -0600

    added packages
    Switched to URP(LWRP)
    Added Cinemachine, Probuilder, URP
    Organized Materials to their own folder.

--- Spring 2020 ClubProject/Assets/Scripts/PlayerController.cs
+++ Spring 2020 ClubProject/Assets/Scripts/PlayerController.cs
@@ -1,46 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed; 
-    private Rigidbody myRigidbody; 
-    private Vector3 moveInput; 
-    private Vector3 moveVelocity; 
+    [SerializeField] private float moveSpeed;
 
-    private Camera mainCamera; 
+    private Rigidbody myRigidbody;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody>(); 
-        mainCamera = FindObjectOfType<Camera>(); 
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed; 
+        moveVelocity = moveInput * moveSpeed;
 
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayLength;
 
-         if(groundPlane.Raycast(cameraRay, out rayLength)){
-          Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-          Debug.Log("you are in the if"); 
-          Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue); 
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.Log("you are in the if");
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
 
-    //        transform.LookAt(pointToLook); 
-         transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-          }
+            //        transform.LookAt(pointToLook); 
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
 
     }
 
-    void FixedUpdate(){
-        myRigidbody.velocity = moveVelocity; 
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
     }
 
 }
