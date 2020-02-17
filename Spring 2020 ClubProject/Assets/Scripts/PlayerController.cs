653fcd15fd602b6b0fafe057237030dde05056bc
Conrad21 <ConradPtasznik@gmail.com>
2020-02-17 15:17:06 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Spring 2020 ClubProject/Assets/Scripts/PlayerController.cs
+++ Spring 2020 ClubProject/Assets/Scripts/PlayerController.cs
@@ -1,47 +1,53 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    public float moveSpeed; 
+    private Rigidbody myRigidbody; 
+    private Vector3 moveInput; 
+    private Vector3 moveVelocity; 
 
-    private Rigidbody myRigidbody;
-    private Vector3 moveInput;
-    private Vector3 moveVelocity;
+    private Camera mainCamera; 
 
-    private Camera mainCamera;
+    public GunControl thegun; 
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody>();
-        mainCamera = FindObjectOfType<Camera>();
+        myRigidbody = GetComponent<Rigidbody>(); 
+        mainCamera = FindObjectOfType<Camera>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
+        moveVelocity = moveInput * moveSpeed; 
 
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayLength;
 
-        if (groundPlane.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.Log("you are in the if");
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+         if(groundPlane.Raycast(cameraRay, out rayLength)){
+          Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+          Debug.Log("you are in the if"); 
+          Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue); 
 
-            //        transform.LookAt(pointToLook); 
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
+    //        transform.LookAt(pointToLook); 
+         transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+          }
 
+          if(Input.GetMouseButtonDown(0))
+            thegun.isFiring = true;
+            
+                     if(Input.GetMouseButtonUp(0))
+            thegun.isFiring = false;
     }
 
-    void FixedUpdate()
-    {
-        myRigidbody.velocity = moveVelocity;
+    void FixedUpdate(){
+        myRigidbody.velocity = moveVelocity; 
     }
 
 }
