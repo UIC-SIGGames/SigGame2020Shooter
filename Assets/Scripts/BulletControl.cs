fbd79e55595d4712858eab5f42ed50eb4bf23315
Chris <55300770+pizzatree@users.noreply.github.com>
2020-02-20 17:20:38 -0600

    Moved project to root
    Removed extraneous folder that made the gitignore not work.

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    [SerializeField] private float speed = 50f,
+                                   despawnTime = 2f;
+
+    IEnumerator despawn()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        StartCoroutine(despawn());
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime );
+    }
+}
