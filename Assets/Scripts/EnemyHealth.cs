fe6e0409020bb349e6f15a8e3592be4a6cb33b80
Conrad21 <ConradPtasznik@gmail.com>
2020-02-24 16:08:24 -0600

    Health bar on stop sign

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float coolDownTime = 4.0f;
+    public float health; 
+    public float maxHealth; 
+
+    public GameObject healthBar; 
+    public Slider mainSlider;
+
+    public GameObject fill; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        mainSlider.value = CalculateHealth(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainSlider.value = CalculateHealth(); 
+        if(health < maxHealth){
+            healthBar.SetActive(true); 
+        }
+         if(health <= 0){
+            fill.SetActive(false); 
+            StartCoroutine(die());
+             
+         }
+
+         if(health >maxHealth){
+             health = maxHealth; 
+         }
+    }
+
+
+ void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet_1" ){
+            health = health - 100; 
+            CalculateHealth(); 
+        }
+    }
+
+
+float  CalculateHealth(){
+        return health / maxHealth; 
+    }
+
+
+IEnumerator die()
+    {
+       
+        yield return new WaitForSeconds(coolDownTime);
+     Destroy(this.gameObject); 
+    }
+
+
+}
+
