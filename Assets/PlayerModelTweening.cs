a677ca4a63608f5356bf54f417e5af613739e3ac
Chris <55300770+pizzatree@users.noreply.github.com>
2020-03-19 13:48:17 -0500

    Added Player Tweening
    Simulates the bob and tilt of an airborne drone.

---
+++ Assets/PlayerModelTweening.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerModelTweening : MonoBehaviour
+{
+    [SerializeField] private Transform model; // can make this an array to do multiple pieces
+
+    [Header("Customizables")]
+    [SerializeField] private float tiltScale = 2f;
+    [SerializeField] private float bobDistance = 1f,
+                                   bobSpeed = 5f;
+
+    private Rigidbody rb;
+    private float baseY;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        baseY = model.position.y;
+    }
+
+    private void Update()
+    {
+        HandleBob();
+        HandleTilt();
+    }
+
+    private void HandleBob()
+    {
+        Vector3 newPos = new Vector3(
+            model.position.x,
+            Mathf.Lerp(baseY - bobDistance, baseY + bobDistance, Mathf.Abs(Mathf.Sin(Time.time * bobSpeed))),
+            model.position.z);
+
+        model.position = newPos;
+    }
+
+    private void HandleTilt()
+    {
+        model.rotation = Quaternion.Euler(new Vector3(
+            rb.velocity.x * tiltScale * transform.forward.x
+           + rb.velocity.z * tiltScale * transform.right.x,
+
+            model.eulerAngles.y, // leave [Britne]y alone -> looks at mouse
+
+            rb.velocity.z * -tiltScale * transform.right.z
+           + rb.velocity.x * -tiltScale * transform.forward.z
+            ));
+    }
+}
